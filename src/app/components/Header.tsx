1df10ee2f7bc72d70d22e38d627483a44981deed
wasique@dynsimulation.com <wasique@dynsimulation.com>
2025-08-05 16:34:48 +0530

    submenu

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -4,7 +4,7 @@ import React, { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { FaFacebook, FaInstagram, FaYoutube } from "react-icons/fa6";
-import { RxCross1, RxHamburgerMenu } from "react-icons/rx";
+import { RxChevronDown, RxCross1, RxHamburgerMenu } from "react-icons/rx";
 
 const Header = () => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
@@ -19,48 +19,93 @@ const Header = () => {
             onClick={() => setIsOpen(!isOpen)}
           />
           <nav className="flex flex-col items-center justify-center h-full space-y-6 text-xl font-light">
-            <a
-              href="#home"
+            <Link
+              href="/"
               className="hover:text-blue-600"
               onClick={() => setIsOpen(false)}
             >
               Home
-            </a>
-            <a
-              href="#about"
+            </Link>
+            <Link
+              href="/"
               className="hover:text-blue-600"
               onClick={() => setIsOpen(false)}
             >
               About Us
-            </a>
-            <a
-              href="#services"
+            </Link>
+            <Link
+              href="/our-services"
               className="hover:text-blue-600"
               onClick={() => setIsOpen(false)}
             >
               Services
-            </a>
-            <a
+            </Link>
+
+            {/* services submene */}
+            <Link
+              href="/research&development"
+              className="hover:text-blue-600"
+              onClick={() => setIsOpen(false)}
+            >
+              R&D
+            </Link>
+            <Link
+              href="/international-business-deveopment"
+              className="hover:text-blue-600"
+              onClick={() => setIsOpen(false)}
+            >
+              Operations
+            </Link>
+            <Link
+              href="/international-business-deveopment"
+              className="hover:text-blue-600"
+              onClick={() => setIsOpen(false)}
+            >
+              Certification
+            </Link>
+            <Link
+              href="/sales&marketing"
+              className="hover:text-blue-600"
+              onClick={() => setIsOpen(false)}
+            >
+              Sales & Mrkt
+            </Link>
+            <Link
+              href="/human-resource"
+              className="hover:text-blue-600"
+              onClick={() => setIsOpen(false)}
+            >
+              HR Consultancy
+            </Link>
+            <Link
+              href="/legal-consulting"
+              className="hover:text-blue-600"
+              onClick={() => setIsOpen(false)}
+            >
+              Legal Advisory
+            </Link>
+            {/* end submenu */}
+            <Link
               href="#customers"
               className="hover:text-blue-600"
               onClick={() => setIsOpen(false)}
             >
               Customers
-            </a>
-            <a
-              href="#gallery"
+            </Link>
+            <Link
+              href="/gallery"
               className="hover:text-blue-600"
               onClick={() => setIsOpen(false)}
             >
               Our Gallery
-            </a>
-            <a
-              href="#contact"
+            </Link>
+            <Link
+              href="/contact"
               className="hover:text-blue-600"
               onClick={() => setIsOpen(false)}
             >
               Contact Us
-            </a>
+            </Link>
           </nav>
         </div>
       )}
@@ -102,9 +147,60 @@ const Header = () => {
             <Link href="/#about" className="mr-0 hover:text-gray-900">
               About Us
             </Link>
-            <Link href="/our-services" className="mr-0 hover:text-gray-900">
-              Services
-            </Link>
+            <div className="relative group">
+              <div className="flex items-center gap-0.5">
+                <Link href="/our-services" className="mr-0 hover:text-gray-900">
+                  Services
+                </Link>
+                <RxChevronDown />
+              </div>
+              {/* Submenu */}
+              <div className="absolute top-full left-0 hidden group-hover:block bg-white mt-1 rounded w-42 pb-4 z-50">
+                <Link
+                  href="/our-services"
+                  className="block px-3 py-3 hover:bg-tertiary"
+                >
+                  Services
+                </Link>
+                <Link
+                  href="/research&development"
+                  className="block px-3 py-3 hover:bg-tertiary"
+                >
+                  R&D
+                </Link>
+                <Link
+                  href="/international-business-deveopment"
+                  className="block px-3 py-3 hover:bg-tertiary"
+                >
+                  Operations
+                </Link>
+                <Link
+                  href="/international-business-deveopment"
+                  className="block px-3 py-3 hover:bg-tertiary"
+                >
+                  Certification
+                </Link>
+                <Link
+                  href="/sales&marketing"
+                  className="block px-3 py-3 hover:bg-tertiary"
+                >
+                  Sales & Mrkt
+                </Link>
+                <Link
+                  href="/human-resource"
+                  className="block px-3 py-3 hover:bg-tertiary"
+                >
+                  HR Consultancy
+                </Link>
+                <Link
+                  href="/legal-consulting"
+                  className="block px-3 py-3 hover:bg-tertiary"
+                >
+                  Legal Advisory
+                </Link>
+              </div>
+            </div>
+
             <Link href="/#customer" className="mr-0 hover:text-gray-900">
               Customers
             </Link>
