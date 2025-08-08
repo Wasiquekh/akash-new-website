0b2ba416fd8c67ccef2801e4ccc5bb219449127d
wasique@dynsimulation.com <wasique@dynsimulation.com>
2025-08-08 16:43:04 +0530

    added images for operation  page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,3 +1,7 @@
+"use client";
+
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
 import React from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
@@ -5,10 +9,42 @@ import { FaFacebookF, FaLinkedin, FaXTwitter } from "react-icons/fa6";
 import Image from "next/image";
 //import ContactFrom from "../components/ContactFrom";
 import { FiInstagram } from "react-icons/fi";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+interface FormValues {
+  name: string;
+  email: string;
+  phone: string;
+  message: string;
+}
+const validationSchema = Yup.object({
+  name: Yup.string().required("Name is required"),
+  email: Yup.string().email("Invalid email").required("Email is required"),
+  phone: Yup.string().required("Phone number is required"),
+  message: Yup.string().required("Message is required"),
+});
 
 const page = () => {
+  const initialValues: FormValues = {
+    name: "",
+    email: "",
+    phone: "",
+    message: "",
+  };
+
+  const onSubmit = (values: FormValues, { resetForm }: any) => {
+    toast.success("Message sent successfully!", {
+      position: "top-right",
+      autoClose: 3000,
+    });
+    console.log(values);
+    resetForm();
+  };
   return (
     <>
+      {/* Toast container renders all the toasts */}
+      <ToastContainer />
       <Header />
       {/*  TOP SECTION Banner */}
       <div className="bg-[url('/images/legal-consulting.jpg')] bg-cover bg-center">
@@ -88,55 +124,108 @@ const page = () => {
               </p>
             </div>
           </div>
+
           <div className="w-full bg-tertiary py-6 px-4 md:px-12 rounded ">
-            <div className="lg:w-full   flex flex-col md:ml-auto w-full md:py-0 mt-8 md:mt-0 h-auto text-sm font-normal ">
-              <div className="relative mb-4">
-                <label htmlFor="name" className="leading-7  ">
-                  Name
-                </label>
-                <input
-                  type="text"
-                  id="name"
-                  name="name"
-                  className="w-full bg-white rounded border border-gray-300  focus:border-tertiary focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-                />
-              </div>
-              <div className="relative mb-4">
-                <label htmlFor="email" className="leading-7  ">
-                  Email
-                </label>
-                <input
-                  type="email"
-                  id="email"
-                  name="email"
-                  className="w-full bg-white rounded border border-gray-300  focus:border-tertiary focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-                />
-              </div>
-              <div className="relative mb-4">
-                <label htmlFor="name" className="leading-7  ">
-                  Phone number
-                </label>
-                <input
-                  type="text"
-                  id="name"
-                  name="name"
-                  className="w-full bg-white rounded border border-gray-300  focus:border-tertiary focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-                />
-              </div>
-              <div className="relative mb-4">
-                <label htmlFor="message" className="leading-7 text-sm  ">
-                  Message
-                </label>
-                <textarea
-                  id="message"
-                  name="message"
-                  className="w-full bg-white rounded border border-gray-300  focus:border-tertiary focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
-                ></textarea>
-              </div>
-              <button className="text-white bg-secondary border-0 py-2 px-6 focus:outline-none hover:bg-primary rounded text-lg">
-                Submit
-              </button>
-            </div>
+            <Formik
+              initialValues={initialValues}
+              validationSchema={validationSchema}
+              onSubmit={onSubmit}
+            >
+              {({ touched, errors }) => (
+                <Form className="lg:w-full flex flex-col md:ml-auto w-full md:py-0 mt-8 md:mt-0 h-auto text-sm font-normal">
+                  <div className="relative mb-4">
+                    <label htmlFor="name" className="leading-7">
+                      Name
+                    </label>
+                    <Field
+                      type="text"
+                      id="name"
+                      name="name"
+                      className={`w-full bg-white rounded border ${
+                        touched.name && errors.name
+                          ? "border-red-500"
+                          : "border-gray-300"
+                      } focus:border-tertiary focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out`}
+                    />
+                    <ErrorMessage
+                      name="name"
+                      component="p"
+                      className="text-red-500 text-xs mt-1 absolute top-17"
+                    />
+                  </div>
+
+                  <div className="relative mb-4">
+                    <label htmlFor="email" className="leading-7">
+                      Email
+                    </label>
+                    <Field
+                      type="email"
+                      id="email"
+                      name="email"
+                      className={`w-full bg-white rounded border ${
+                        touched.email && errors.email
+                          ? "border-red-500"
+                          : "border-gray-300"
+                      } focus:border-tertiary focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out`}
+                    />
+                    <ErrorMessage
+                      name="email"
+                      component="p"
+                      className="text-red-500 text-xs mt-1 absolute top-17"
+                    />
+                  </div>
+
+                  <div className="relative mb-4">
+                    <label htmlFor="phone" className="leading-7">
+                      Phone number
+                    </label>
+                    <Field
+                      type="text"
+                      id="phone"
+                      name="phone"
+                      className={`w-full bg-white rounded border ${
+                        touched.phone && errors.phone
+                          ? "border-red-500"
+                          : "border-gray-300"
+                      } focus:border-tertiary focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out`}
+                    />
+                    <ErrorMessage
+                      name="phone"
+                      component="p"
+                      className="text-red-500 text-xs mt-1 absolute top-17"
+                    />
+                  </div>
+
+                  <div className="relative mb-4">
+                    <label htmlFor="message" className="leading-7 text-sm">
+                      Message
+                    </label>
+                    <Field
+                      as="textarea"
+                      id="message"
+                      name="message"
+                      className={`w-full bg-white rounded border ${
+                        touched.message && errors.message
+                          ? "border-red-500"
+                          : "border-gray-300"
+                      } focus:border-tertiary focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out`}
+                    />
+                    <ErrorMessage
+                      name="message"
+                      component="p"
+                      className="text-red-500 text-xs mt-1 absolute top-38"
+                    />
+                  </div>
+
+                  <button
+                    type="submit"
+                    className="text-white bg-secondary border-0 py-2 px-6 focus:outline-none hover:bg-primary rounded text-lg"
+                  >
+                    Submit
+                  </button>
+                </Form>
+              )}
+            </Formik>
           </div>
         </div>
       </section>
---
+++ src/app/operation/page.tsx
@@ -0,0 +1,193 @@
+import React from "react";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import Image from "next/image";
+import ContactFrom from "../components/ContactFrom";
+
+const page = () => {
+  return (
+    <>
+      <Header />
+      {/*  TOP SECTION Banner */}
+      <div className="bg-[url('/images/services.jpg')] bg-cover bg-center">
+        <div className=" max-w-7xl m-auto px-5  py-40">
+          <h1 className="sm:text-6xl text-2xl font-bold text-white mb-4 text-center ">
+            Operations Consulting
+          </h1>
+          <p className=" mx-auto leading-relaxed text-sm font-normal text-white inter-text text-center">
+            International Business Development & Logistics Services
+          </p>
+        </div>
+      </div>
+      <section className=" relative">
+        <Image
+          src="/images/leftShape.svg" // path relative to /public
+          alt="My beautiful image"
+          width={300}
+          height={300}
+          priority // optional: preloads image
+          className=" absolute left-0 top-0 "
+        />
+        <Image
+          src="/images/rightShape.svg" // path relative to /public
+          alt="My beautiful image"
+          width={300}
+          height={300}
+          priority // optional: preloads image
+          className=" absolute right-0 top-0 "
+        />
+      </section>
+      <section>
+        <div className="container">
+          <div className=" flex flex-wrap">
+            <div className="flex flex-col text-center w-full mb-20">
+              <h1 className="sm:text-6xl text-2xl font-bold  mb-4 text-primary">
+                Short Term Courses
+              </h1>
+              <p className="lg:w-[90%] mx-auto leading-relaxed inter-text text-sm font-normal mb-4">
+                We are one of the end to end service provider company. After
+                successfully working in Design, New product Development, Lean
+                Manufacturing, HR and Marketing sector we start “International
+                Business Development And Logistics” Services for our valuable
+                customer. 
+              </p>
+            </div>
+          </div>
+          <div className=" grid grid-cols-1 md:grid-cols-2 gap-4">
+            <div className="flex items-center bg-[#FEF2FB] gap-4 py-4 px-6 rounded">
+              <div>
+                <Image
+                  src="/images/Lean Manufacturing.png" // path relative to /public
+                  alt="My beautiful image"
+                  width={130}
+                  height={130}
+                  priority // optional: preloads image
+                  className=""
+                />
+              </div>
+
+              <div>
+                <p className=" text-2xl font-medium text-black  mb-1">
+                  Lean Manufacturing
+                </p>
+                <p className=" text-sm font-normal text-black inter-text">
+                  Basic Level, Intermediate Level, Advance Level
+                </p>
+              </div>
+            </div>
+            <div className="flex items-center bg-[#FEF2FB] gap-4 py-4 px-6 rounded">
+              <div>
+                <Image
+                  src="/images/Zed Certification.png" // path relative to /public
+                  alt="My beautiful image"
+                  width={130}
+                  height={130}
+                  priority // optional: preloads image
+                  className=""
+                />
+              </div>
+
+              <div>
+                <p className=" text-2xl font-medium text-black  mb-1">
+                  ZED CERTIFICATION
+                </p>
+                <p className=" text-sm font-normal text-black inter-text">
+                  Bronze certification, Silver certification, Gold Certification
+                </p>
+              </div>
+            </div>
+            <div className="flex items-center bg-[#FEF2FB] gap-4 py-4 px-6 rounded">
+              <div>
+                <Image
+                  src="/images/Cost Innovation Module.png" // path relative to /public
+                  alt="My beautiful image"
+                  width={130}
+                  height={130}
+                  priority // optional: preloads image
+                  className=""
+                />
+              </div>
+
+              <div>
+                <p className=" text-2xl font-medium text-black  mb-1">
+                  Cost Innovation Module
+                </p>
+                <p className=" text-sm font-normal text-black inter-text">
+                  You can edit this text for your presentation
+                </p>
+              </div>
+            </div>
+            <div className="flex items-center bg-[#FEF2FB] gap-4 py-4 px-6 rounded">
+              <div>
+                <Image
+                  src="/images/Rapid Supply China Improvement.png" // path relative to /public
+                  alt="My beautiful image"
+                  width={130}
+                  height={130}
+                  priority // optional: preloads image
+                  className=""
+                />
+              </div>
+
+              <div>
+                <p className=" text-2xl font-medium text-black  mb-1">
+                  Rapid Supply China Improvement
+                </p>
+                <p className=" text-sm font-normal text-black inter-text">
+                  You can edit this text for your presentation
+                </p>
+              </div>
+            </div>
+            <div className="flex items-center bg-[#FEF2FB] gap-4 py-4 px-6 rounded">
+              <div>
+                <Image
+                  src="/images/Third Party Audit.png" // path relative to /public
+                  alt="My beautiful image"
+                  width={130}
+                  height={130}
+                  priority // optional: preloads image
+                  className=""
+                />
+              </div>
+
+              <div>
+                <p className=" text-2xl font-medium text-black  mb-1">
+                  Third Party Audit
+                </p>
+                <p className=" text-sm font-normal text-black inter-text">
+                  You can edit this text for your presentation
+                </p>
+              </div>
+            </div>
+            <div className="flex items-center bg-[#FEF2FB] gap-4 py-4 px-6 rounded">
+              <div>
+                <Image
+                  src="/images/100 Day Renovation Program.png" // path relative to /public
+                  alt="My beautiful image"
+                  width={130}
+                  height={130}
+                  priority // optional: preloads image
+                  className=""
+                />
+              </div>
+
+              <div>
+                <p className=" text-2xl font-medium text-black  mb-1">
+                  100 Day Renovation Program
+                </p>
+                <p className=" text-sm font-normal text-black inter-text">
+                  You can edit this text for your presentation
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <ContactFrom />
+      <Footer />
+    </>
+  );
+};
+
+export default page;
