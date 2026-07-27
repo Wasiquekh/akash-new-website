81063413b4b6c6ce71048c1891938e9e73a19e48
prashik2002 <workwith.prashik@gmail.com>
2026-07-27 19:35:03 +0530

    Add Microsoft Clarity tracking script for enhanced user analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -44,6 +44,17 @@ export default function RootLayout({
           `}
         </Script>
 
+        {/* ✅ Microsoft Clarity */}
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "xt17t4a97g");
+          `}
+        </Script>
+
         {/* ✅ Sitemap link for crawlers */}
         <link rel="sitemap" type="application/xml" href="https://www.asbconsulting.in/sitemap.xml" />
       </head>
